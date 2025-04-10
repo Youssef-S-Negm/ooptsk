a9a2d6807f61216c0bd51ef87a5a68ab62e88673
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-10 12:30:48 +0200

    initial commit

---
+++ src/main/java/com/youssef/quiz/QuizApplication.java
@@ -0,0 +1,10 @@
+package com.youssef.quiz;
+
+/**
+ * Hello world!
+ */
+public class QuizApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
